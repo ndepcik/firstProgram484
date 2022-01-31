0223feb1613bc708c64d7fb44d6f139c74f01e54
depcik <z1892457@students.niu.edu>
2022-01-31 10:33:50 -0600

    Euler intgration in place

--- SimPen.cs
+++ SimPen.cs
@@ -10,33 +10,47 @@ namespace Sim
 {
     public class SimPen
     {
-        private double leng = 1.1; // length of pendulum
+        private double leng = 1.1;  // length of pendulum
         private double g = 9.81;    // gravity
+
+        private int n = 2;          //num of states
+        private double[] x;         //array of states
+        private double[] f;         //right side of eq evaluated
+        
 
         //const
         public SimPen()
         {
-            Console.WriteLine("svilen is small");
+            x = new double [n];
+            f = new double [n];
+
+            x[0] = 1.0;
+            x[1] = 0.0;
+
+        }
+        /*methods ************************************************************
+            step : perform intgration
+            rhsFunc: function to calculate
+        */
+        public void step(double dt)
+        {
+            rhsFunc(x,f);
+            
+            for(int i=0; i<n; i++)
+            {
+                x[i] = x[i] +f[i] * dt;
+            }
+            // Console.WriteLine($"x: {x[0].ToString()}   {x[1].ToString()}"  );
+            // Console.WriteLine($"f: {f[0].ToString()}   {f[1].ToString()}"  );
         }
 
-        /*public double getLeng()
+        public void rhsFunc(double[] st, double[] ff)
         {
-            return leng;
+            ff[0] = st[1];
+            ff[1] = -g/leng * Math.Sin(st[0]);
         }
-        public double getG()
-        {
-            return g;
-        }        
-        public void setLeng(double l)
-        {
-            if(l > 0)
-                leng = l;
-        }
-        public void setG(double g)
-        {
-            g = this.g;
-        }
-        */
+
+       //getter and setter ***************************************************
         public double L
         {
             get {return(leng);}
@@ -48,7 +62,7 @@ namespace Sim
             }
         }
 
-         public double G
+        public double G
         {
             get {return(g);}
 
@@ -58,5 +72,21 @@ namespace Sim
                     g = value;
             }
         }
+        
+        public double theta
+        {
+            get { return x[0]; }
+
+            set { x[0] = value; }
+        }
+
+        public double thetaDot
+        {
+            get { return x[1]; }
+
+            set { x[1] = value; }
+        }
+
+
     }
 }
